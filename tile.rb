47de795f44143f10edeaf5490dc9c77dc4d5498a
Robby Balistreri <rbalistreri5@gmail.com>
2021-03-11 16:15:47 -0600

    Add functionality to reveal adjacent tiles upon selection

--- tile.rb
+++ tile.rb
@@ -20,7 +20,29 @@ class Tile
             #SOMETHING ELSE TO END THE GAME
         else
             @adjacent_bombs = adjacent_bombs
+            if @adjacent_bombs == 0
+                row_min = @position[0]-1
+                row_max = @position[0]+1
+                col_min = @position[1]-1
+                col_max = @position[1]+1
+
+                row_min = 0 if row_min < 0
+                col_min = 0 if col_min < 0
+                row_max = 8 if row_max > 8
+                col_max = 8 if col_max > 8
+
+                row_range = (row_min..row_max)
+                col_range = (col_min..col_max)
+                row_range.each do |row|
+                    col_range.each do |col|
+                        if !@board[[row,col]].is_revealed
+                            @board[[row,col]].reveal
+                        end
+                    end
+                end
+            end
         end
+        
     end
 
     def cheat_reveal
